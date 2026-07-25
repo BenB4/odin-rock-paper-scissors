5c996ee572dbf8554bbb15b753e133d881129da8
Benjamin Baird <benjaminrbaird@gmail.com>
2026-07-25 15:47:43 -0400

    finished user prompt function and function to play one rpc round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,5 @@
-console.log("test")
+let userScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choiceNumber = Math.floor(Math.random() * 3);
@@ -6,3 +7,43 @@ function getComputerChoice() {
         choiceNumber == 1 ? "scissors" :
         "paper";
 }
+
+function getUserChoice() {
+    let userChoice;
+    let validChoice = false;
+    while (!validChoice) {
+        userChoice = prompt("choose rock, paper, or scissors. Or test your luck!")
+        if (userChoice != null) {
+            validChoice = userChoice.toLowerCase() == "rock"
+            || userChoice.toLowerCase() == "paper"
+            || userChoice.toLowerCase() == "scissors";
+            if (userChoice == "") {return getComputerChoice()}
+        }
+    }
+    return userChoice;
+}
+
+
+
+function playRound(userChoice, computerChoice) {
+    let winMsg = "You win! " + userChoice + " beats " + computerChoice;
+    let loseMsg = "You Lose! " + userChoice + " loses to " + computerChoice;
+    let tieMsg = "Tie! " + userChoice + " ties with " + computerChoice;
+    if (userChoice == computerChoice) {
+        console.log(tieMsg)
+        return null
+    } else if (userChoice == "rock") {
+        console.log(computerChoice == "paper" ? loseMsg : winMsg)
+        return computerChoice == "paper" ? false : true
+    } else if (userChoice == "paper") {
+        console.log(computerChoice == "paper" ? loseMsg : winMsg)
+        return computerChoice == "scissors" ? false : true
+    } else {
+        console.log(computerChoice == "paper" ? loseMsg : winMsg)
+        return computerChoice == "rock" ? false : true
+    }
+}
+
+
+let userChoice = getUserChoice();
+let computerScore = getComputerChoice();
